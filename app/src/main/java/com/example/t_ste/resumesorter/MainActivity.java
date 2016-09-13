d615da1c7edfe6a8bd607b385bcb6efa1b862f4d
trevor stephens <trevor stephens>
2016-09-13 14:27:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/t_ste/resumesorter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.t_ste.resumesorter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
